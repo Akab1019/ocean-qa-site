c395d89ca04333409ffadede5eb7b946a94cfaee
Akab1019 <3869934779@qq.com>
2025-06-06 09:59:09 +0800

    更新 ask_api.py，优化端口和健壮性

--- ask_api.py
+++ ask_api.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
-# 导入你的问答函数和向量库
 from ask_question import ask_question
 
 app = Flask(__name__)
-CORS(app)  # 允许跨域
+CORS(app)
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    data = request.json
-    question = data.get('question')
+    data = request.get_json()
+    question = data.get('question', '')
     answer = ask_question(question)
     return jsonify({'answer': answer})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
